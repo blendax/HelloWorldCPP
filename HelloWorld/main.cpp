83e831655102e8fb65b8bfe75137fc0443187f43
Mikael Hermansson <mikael.hermansson@tradedoubler.com>
2014-01-01 21:24:40 +0100

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Mikael Hermansson on 2014-01-01.
+//  Copyright (c) 2014 Mikael Hermansson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
